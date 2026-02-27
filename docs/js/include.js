a3d860b35cf0f014ac16da311f8e1cf301ad2218
macho6666 <macho0514@gmail.com>
2026-02-28 01:34:48 +0900

    Update include.js

--- docs/js/include.js
+++ docs/js/include.js
@@ -1,3 +1,21 @@
+
+import './viewer_modules/core/state.js';
+import './viewer_modules/core/events.js';
+import './viewer_modules/core/utils.js';
+import './viewer_modules/text/text_state.js';
+import './viewer_modules/text/text_theme.js';
+import './viewer_modules/text/text_toc.js';
+import './viewer_modules/text/text_bookmark.js';
+import './viewer_modules/text/text_renderer.js';
+import './viewer_modules/text/epub_renderer.js';
+import './viewer_modules/text/text_navigation.js';
+import './viewer_modules/text/text_controls.js';
+import './viewer_modules/text/text_highlight.js';
+import './viewer_modules/text/index.js';
+import './viewer_modules/fetcher.js';
+import './viewer_modules/actions.js';
 import './viewer_modules/index.js';
-import './main.js';
 import './api_client.js';
+import './main.js';
+
+console.log('✅ All Modules Loaded via include.js');
