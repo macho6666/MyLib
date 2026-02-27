08bd86d28ae4a510aec3149b31ac96915e77efa2
macho6666 <macho0514@gmail.com>
2026-02-28 02:17:01 +0900

    Update include.js

--- docs/js/include.js
+++ docs/js/include.js
@@ -1,7 +1,14 @@
+/**
+ * docs/js/include.js
+ * 모든 뷰어 모듈 통합 로드
+ */
 
+// ===== Core =====
 import './viewer_modules/core/state.js';
 import './viewer_modules/core/events.js';
 import './viewer_modules/core/utils.js';
+
+// ===== Text Viewer =====
 import './viewer_modules/text/text_state.js';
 import './viewer_modules/text/text_theme.js';
 import './viewer_modules/text/text_toc.js';
@@ -12,9 +19,22 @@ import './viewer_modules/text/text_navigation.js';
 import './viewer_modules/text/text_controls.js';
 import './viewer_modules/text/text_highlight.js';
 import './viewer_modules/text/index.js';
+
+// ===== Image Viewer =====
+import './viewer_modules/image/image_state.js';
+import './viewer_modules/image/image_renderer.js';
+import './viewer_modules/image/image_navigation.js';
+import './viewer_modules/image/image_controls.js';
+import './viewer_modules/image/index.js';
+
+// ===== Fetcher & Actions =====
 import './viewer_modules/fetcher.js';
 import './viewer_modules/actions.js';
+
+// ===== Main Index =====
 import './viewer_modules/index.js';
+
+// ===== 기존 파일들 =====
 import './api_client.js';
 import './main.js';
 
